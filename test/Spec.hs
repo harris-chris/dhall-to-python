3156132c0e6466303850a4b7ddc7a5ab2b48a21a
harris-chris <chrisharriscjh@gmail.com>
2022-10-15 11:48:31 +0900

    Parsing file test passing

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,7 +1,17 @@
 import Test.Hspec
+import Test.HUnit.Lang (assertFailure)
+import Debug.Trace (traceShowId)
 import DhallToPythonLib
-import Dhall.Parser
 import Dhall.Core
+import Dhall.Parser
+import Dhall.Src
+import qualified Data.Text as T
+import qualified Data.Text.IO as TIO
+import Text.Megaparsec (SourcePos(..), mkPos)
+import System.FilePath ((</>))
+
+testData :: FilePath
+testData = "test/test_files"
 
 main :: IO ()
 main = hspec $ do
@@ -10,8 +20,25 @@ main = hspec $ do
             let expected = NaturalLit 21 :: Expr Src Import
             let actual = exprFromText "Simple Natural" "21" :: Either ParseError (Expr Src Import)
             case actual of
-                Left err -> 1 `shouldBe` 0
-                Right expr -> case expr of
-                    Note _ val -> val `shouldBe` expected
-                    _ -> 1 `shouldBe` 0
+                Left err -> assertFailure "Could not parse"
+                Right ( Note _ val ) -> val `shouldBe` expected
+                Right _ -> assertFailure "Expected not actual"
+
+    describe "Read and parse a dhall file" $ do
+        it "parses a file with a natural number" $ do
+            let fpath = testData </> "natural.dhall" :: FilePath
+            contents <- TIO.readFile fpath
+            let expected = NaturalLit 22
+            let actual = exprFromText fpath contents
+            case actual of
+                Left _ -> assertFailure "Expression could not be parsed"
+                Right (Note src (Note src' val)) -> do
+                    src `shouldBe` Src
+                        (SourcePos fpath (mkPos 1) (mkPos 1))
+                        (SourcePos fpath (mkPos 2) (mkPos 1))
+                        "22\n"
+                    val `shouldBe` expected
+                Right _ -> assertFailure "Parsed expression was not expected"
+
+
 
